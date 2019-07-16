4b70708d279539915841b9b6a0b4d3ae196b4fd8
RakshitDJoshi <33176682+RakshitDJoshi@users.noreply.github.com>
2019-07-16 10:59:09 +0530

    Update views.py

--- buttonpython/views.py
+++ buttonpython/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from django.core.files.storage import FileSystemStorage
 
+path = 'a'
 
 #to render the page
 def button(request):
     return render(request,'home.html')
+
+def upload(request):
+	print("I was here 1")
+	if request.method == 'POST':
+		uploaded_file = request.FILES['opt_file']
+		fs = FileSystemStorage()
+		name = fs.save(uploaded_file.name, uploaded_file)
+		global path
+		path = fs.path(name)
+	return render(request, 'home.html')
 
 
 #to render the page when button pressed and output is put on the same page
@@ -13,12 +25,13 @@ def execute_scriptJson(request):
     from selenium import webdriver
     import clipboard    #for pasting copied instance
 
-    driver = webdriver.Chrome("/home/saksham/chromedriver")
+    driver = webdriver.Chrome(executable_path = "/usr/bin/chromedriver")
     driver.get("https://server001.cloudehrserver.com/cot/opt/json_instance_generator")
 
     fileinput = driver.find_element_by_id("validatedCustomFile")
     #get path of opt from system
-    path = "/home/saksham/Desktop/Internship(3rd year)/Automating Clinical Instance generation/vital_signs_summary.en.v1.opt"
+    global path 
+    #path = "/home/rakshit/Documents/openEHR/vital_signs_summary.en.v1.opt"
     fileinput.send_keys(path)
 
     submitbutton = driver.find_element_by_name("doit")
@@ -38,12 +51,12 @@ def execute_scriptForm(request):
     from selenium import webdriver
     import clipboard    #for pasting copied instance
 
-    driver = webdriver.Chrome("/home/saksham/chromedriver")
+    driver = webdriver.Chrome(executable_path = "/usr/bin/chromedriver")
     driver.get("https://server001.cloudehrserver.com/cot/opt/html_form_generator")
 
     fileinput = driver.find_element_by_id("validatedCustomFile")
     #get path of opt from system
-    path = "/home/saksham/Desktop/Internship(3rd year)/Automating Clinical Instance generation/vital_signs_summary.en.v1.opt"
+    global path#path = "/home/rakshit/Documents/openEHR/vital_signs_summary.en.v1.opt"
     fileinput.send_keys(path)
 
     submitbutton = driver.find_element_by_name("doit")
